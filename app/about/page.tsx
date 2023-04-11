a8184a114048cb8b698ba36864c02866348200bb
Samuel Pokam <dixiades@gmail.com>
2023-04-11 18:54:20 +0100

    Repo and RepoDir

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About Quanta Hive</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga, quidem
+        ducimus autem amet quod ab tempore at quisquam possimus! Modi
+        laboriosam, voluptas tenetur blanditiis natus fugiat. Obcaecati sequi
+        rem nesciunt?
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ app/about/team/page.tsx
@@ -0,0 +1,15 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
+        culpa eius, laboriosam sunt quaerat itaque nesciunt, ipsa perspiciatis
+        provident fuga fugiat eveniet tempore id amet alias dicta? Excepturi,
+        est eaque.
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
